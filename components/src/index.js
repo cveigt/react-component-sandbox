a43afcf5437cafc7ced3824a062fc8da12f280ab
cveigt <cveigt@gmail.com>
2021-07-17 21:37:38 -0700

    feat(added props)
    Made comment component reusable and added props.

--- components/src/index.js
+++ components/src/index.js
@@ -1,24 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText="This is another long blog post comment." avatar={faker.image.avatar()} />
+      <CommentDetail author="Alex" timeAgo="Today at 2:00 AM" commentText="This is cool stuff, keep it up!" avatar={faker.image.avatar()} />
+      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" commentText="Nice job." avatar={faker.image.avatar()} />
     </div>
   )
 };
