f7325ae5a9841016645119cc8f9fdc1498bcfbc4
cveigt <cveigt@gmail.com>
2021-07-22 16:22:40 -0700

    nit(refactor functions)
    Moved all function to seasonConfig

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import SemanticUI from "semantic-ui-css/semantic.min.css";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -22,7 +24,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading!</div>;
