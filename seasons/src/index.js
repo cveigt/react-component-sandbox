e67ab66f925cd2dbba9a990ed5c4f9e9eaff6ce1
cveigt <cveigt@gmail.com>
2021-07-22 19:22:19 -0700

    feat(add loader component)

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 import SemanticUI from "semantic-ui-css/semantic.min.css";
 
 class App extends React.Component {
@@ -17,8 +18,7 @@ class App extends React.Component {
     console.log("My component was just updated - it rerendered!");
   }
 
-  // React says we have to define render!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -27,7 +27,11 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!</div>;
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
