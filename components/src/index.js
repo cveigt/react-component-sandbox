4550fa880ff4a85a379f280656a1d8a39604c230
cveigt <cveigt@gmail.com>
2021-10-20 22:38:18 -0700

    Created ApprovalCard component

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author="Sam"
-        timeAgo="Today at 6:00PM"
-        content="Nice post"
-      />
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author="Alex"
-        timeAgo="Yesterday"
-        content="Cool and nice reading"
-      />
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author="Jane"
-        timeAgo="A month ago"
-        content="This is a very long comment for this blog post."
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning:</h4>
+          <p>Are you sure to delete?</p>
+        </div>
+      </ApprovalCard>
+      <ApprovalCard></ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Sam"
+          timeAgo="Today at 6:00PM"
+          content="Nice post"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Alex"
+          timeAgo="Yesterday"
+          content="Cool and nice reading"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Jane"
+          timeAgo="A month ago"
+          content="This is a very long comment for this blog post."
+        />
+      </ApprovalCard>
     </div>
   );
 };
